daff63f431ee968c6fdb25683c77dfecd146b79e
wfenglund <72128748+wfenglund@users.noreply.github.com>
2023-11-09 19:07:53 +0100

    Update bgsetter.sh

--- .config_scripts/bgsetter.sh
+++ .config_scripts/bgsetter.sh
@@ -1,34 +1,35 @@
-current_ws=$(/bin/i3-msg -t get_workspaces | /home/william/anaconda3/bin/jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-
-echo "thisworked" > ~/output.test
+old_ws="blank"
 
 while true
 do
 	current_ws=$(/bin/i3-msg -t get_workspaces | /home/william/anaconda3/bin/jq '.[] | select(.focused==true).name' | cut -d"\"" -f2)
-	echo $current_ws >> ~/output.test
-	if [[ $current_ws == "1:Hartnell" ]]
+	if [[ $current_ws != $old_ws ]]
 	then
-		feh --bg-fill ~/Bilder/doctor_1.jpg
-	elif [[ $current_ws == "2:Throughton" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_2.jpg
-	elif [[ $current_ws == "3:Pertwee" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_3.jpg
-	elif [[ $current_ws == "4:Baker1" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_4.jpg
-	elif [[ $current_ws == "5:Davison" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_5.jpg
-	elif [[ $current_ws == "6:Baker2" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_6.jpg
-	elif [[ $current_ws == "7:McCoy" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_7.jpg
-	elif [[ $current_ws == "8:McGann" ]]
-	then
-		feh --bg-fill ~/Bilder/doctor_8.jpg
+		if [[ $current_ws == "1:Hartnell" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_1/*
+		elif [[ $current_ws == "2:Throughton" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_2/*
+		elif [[ $current_ws == "3:Pertwee" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_3/*
+		elif [[ $current_ws == "4:Baker1" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_4/*
+		elif [[ $current_ws == "5:Davison" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_5/*
+		elif [[ $current_ws == "6:Baker2" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_6/*
+		elif [[ $current_ws == "7:McCoy" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_7/*
+		elif [[ $current_ws == "8:McGann" ]]
+		then
+			feh --bg-fill --randomize ~/Bilder/backgrounds/doctor_8/*
+		fi
 	fi
+	old_ws=$current_ws
 done
