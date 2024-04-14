e14fee524b4130002f807055f7c34e22f00be8a3
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-04-15 01:44:28 +0200

    restructure and fix bugs when history does not exist

--- .config_scripts/getbc.py
+++ .config_scripts/getbc.py
@@ -1,10 +1,16 @@
 import requests
 import sys
 from datetime import datetime
-from os import path
+import os.path
 
-file_dir = path.dirname(__file__)
+# Get path to this script:
+file_dir = os.path.dirname(__file__)
 
+# Create history file if it does not exist:
+if os.path.isfile(file_dir + '/history.file') == False:
+    open(file_dir + '/history.file', 'a').close()
+
+# Process flags:
 try:
     choice = sys.argv[1]
     if choice == '-setch':
@@ -13,7 +19,7 @@ try:
             checkpoint = ''.join(filter(str.isdigit, checkpoint))
             if checkpoint == '':
                 checkpoint = '0'
-            with open('checkpoint.file', 'w') as chckpntfl:
+            with open(file_dir + '/checkpoint.file', 'w') as chckpntfl:
                 chckpntfl.writelines(checkpoint)
             print(f'New price checkpoint set to {checkpoint} USD.')
             quit()
@@ -23,39 +29,44 @@ try:
     if choice == '-h':
         print('Prices the previous five times you asked:')
         with open(file_dir + '/history.file', 'r') as history:
+            counter = 0
             for line in history:
+                counter = counter + 1
                 line = line.replace('\n', '')
-                print(line)
+                if counter < 6:
+                    print(line)
+                else:
+                    break
         print()
 except IndexError:
     choice = 'nothing'
     pass
 
+# Get Bitcoin price:
 response = requests.get('https://api.coindesk.com./v1/bpi/currentprice.json')
 data = response.json()
 usd_price = data['bpi']['USD']['rate'].replace(',', '')
 usd_price = float(usd_price)
 usd_price = int(usd_price)
 
+# Write the price and date to the history file:
+now = datetime.now()
+current_time = now.strftime('%Y-%m-%d %H:%M:%S')
+with open(file_dir + '/history.file', 'r+') as history:
+    content = history.read()
+    history.seek(0, 0)
+    history.write(f'{usd_price} [{current_time}]\n' + content)
+
+# Simplify output if user chose 'simple':
 if choice == '-s':
     print(usd_price)
 else:
     print(f'The current price for one Bitcoin is {usd_price} USD.')
 
-now = datetime.now()
-current_time = now.strftime('%Y-%m-%d %H:%M:%S')
-
-with open(file_dir + '/history.file', 'r+') as history:
-    throw = history.readline()
-    data = history.read()
-    history.seek(0)
-    history.write(data)
-    history.writelines(f'{usd_price} [{current_time}]\n')
-    history.truncate()
-
+# Compare price to set checkpoint if user chose 'checkpoint':
 if choice == '-c':
     try:
-        with open('checkpoint.file', 'r') as chkpntfl:
+        with open(file_dir + '/checkpoint.file', 'r') as chkpntfl:
             checkpoint = int(chkpntfl.readline())
         percentage = int(usd_price) / checkpoint
 
