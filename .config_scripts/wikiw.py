b852449a1f049939db5761185c0c244a69f148fb
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-05-30 01:41:32 +0200

    changed from a system with a header and several lines of info

--- .config_scripts/wikiw.py
+++ .config_scripts/wikiw.py
@@ -1,15 +1,23 @@
 import os
 from sys import argv
 
-wikiw_dict = {}
+wikiw_d = {}
+key = 'empty'
 with open(os.path.dirname(__file__) + '/wikiw.txt') as archive:
-    for entry in archive:
-        if entry.startswith('#'):
+    for raw_entry in archive:
+        entry = raw_entry.strip()
+        if entry.startswith('#') or len(entry) == 0:
             continue
-        entry_list = entry.strip().split(':::')
-        wikiw_dict[entry_list[0]] = entry_list[1]
+        if entry.startswith(':::') and entry.endswith(':::'):
+            key = entry.replace(':::', '')
+        else:
+            wikiw_d[key] = wikiw_d[key] + [entry] if key in wikiw_d.keys() else [entry]
 
 if len(argv) > 1:
     query = argv[1]
-    if query in wikiw_dict.keys():
-        print(wikiw_dict[query])
+    hits = [i for i in wikiw_d.keys() if query in i]
+    for key in hits:
+        print(f'{key}:')
+        for line in wikiw_d[key]:
+            print(line)
+        print()
