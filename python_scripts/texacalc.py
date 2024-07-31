7d2fa2915ef9ab5d3db4902deb374f34bdb779f5
William Englund <w.f.englund@gmail.com>
2024-07-31 23:43:00 +0200

    add so that program catches card combinations not in dict

--- python_scripts/texacalc.py
+++ python_scripts/texacalc.py
@@ -13,7 +13,10 @@ hole_dict = {'AA':'1', 'KK':'2', 'QQ':'3', 'AKs':'4', 'JJ':'5', 'AQs':'6', 'KQs'
 def calculate_hand():
     hand = input('What are your hole cards? ')
     if hand != 'q':
-        print(f'{hand} hole card rank: {hole_dict[hand]}')
+        if hand in hole_dict.keys():
+            print(f'{hand} hole card rank: {hole_dict[hand]}')
+        else:
+            print('Card combination not found in database.')
     else:
         return 'quit'
 
