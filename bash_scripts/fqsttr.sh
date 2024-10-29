b977d29ec394f62b0dbf00c4f2fcfcd8498d8403
William Englund <w.f.englund@gmail.com>
2024-10-29 19:42:00 +0100

    fixed so that fqsttr can take several inputs

--- bash_scripts/fqsttr.sh
+++ bash_scripts/fqsttr.sh
@@ -1,8 +1,18 @@
 get_stats() {
   first_seq=`zcat ./$1 | head -n 2 | tail -n 1`
   count_seq=`zgrep "^@" ./$1 | wc -l`
-  echo "Read length:,"${#first_seq}
-  echo "Sequence count:,"$count_seq
+  echo "- Read length:,"${#first_seq}
+  echo "- Sequence count:,"$count_seq
 }
 
-get_stats $1 | column -s ',' -t
+counter=0
+for arg # for every argument
+do
+  if [ $counter -gt 0 ]
+  then
+    echo ""
+  fi
+  echo $arg
+  get_stats $arg | column -s ',' -t
+  counter=$((counter + 1))
+done
